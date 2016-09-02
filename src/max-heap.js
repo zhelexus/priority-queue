9352a32322415d23e77b6ead03faff3249074076
zhelexus <unbelievable_89_@mail.ru>
2016-09-02 16:24:14 +0300

    Create max-heap.js

---
+++ src/max-heap.js
@@ -0,0 +1,51 @@
+const Node = require('./node');
+
+class MaxHeap {
+	constructor() {
+		this.root = null;
+		this.parentNodes = [];
+	}
+
+	push(data, priority) {
+
+	}
+
+	pop() {
+
+	}
+
+	detachRoot() {
+
+	}
+
+	restoreRootFromLastInsertedNode(detached) {
+
+	}
+
+	size() {
+
+	}
+
+	isEmpty() {
+
+	}
+
+	clear() {
+		this.root = null;
+		parentNodes = [];
+	}
+
+	insertNode(node) {
+
+	}
+
+	shiftNodeUp(node) {
+
+	}
+
+	shiftNodeDown(node) {
+
+	}
+}
+
+module.exports = MaxHeap;
